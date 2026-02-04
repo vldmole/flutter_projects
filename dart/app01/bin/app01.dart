686977692bd3c84dbc7375ea88acaf891b7e0cf9
Vilson Luiz Dalle Mole <vldmole@gmail.com>
2026-02-04 11:29:59 -0300

    Branche 'dart' created - dart app01 - hello world created

---
+++ dart/app01/bin/app01.dart
@@ -0,0 +1,5 @@
+import 'package:app01/app01.dart' as app01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app01.calculate()}!');
+}
---
+++ dart/app01/test/app01_test.dart
@@ -0,0 +1,8 @@
+import 'package:app01/app01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
