e7581a944974725d020aa55e2371c94bcb8e5290
Vilson Luiz Dalle Mole <vldmole@gmail.com>
2026-02-10 11:10:00 -0300

    GoogleFonts - changed some component style with a different font

--- app02/lib/pages/home_page.dart
+++ app02/lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 //stf tab
 class MyWidgetHomePage extends StatefulWidget {
@@ -17,9 +18,15 @@ class _MyWidgetHomePageState extends State<MyWidgetHomePage> {
   final _olaText = Text(
       "Olá", 
       textDirection: TextDirection.rtl, 
-      textScaler: TextScaler.linear(2)
+      textScaler: TextScaler.linear(2),
+      style: GoogleFonts.abyssinicaSil(fontSize: 30),
     );
   
+  final _appBar =  AppBar(
+        title: Text("Generator of Random Integer Numbers", style: GoogleFonts.pacifico()),
+        elevation: 1,
+      );
+ 
   final _randomGen = Random();
   int _generatedInt = 0;
   
@@ -27,17 +34,11 @@ class _MyWidgetHomePageState extends State<MyWidgetHomePage> {
     return Text(
       _generatedInt.toString(),
       textScaler: TextScaler.linear(5),
+      style: GoogleFonts.aladin(),
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Generator of Random Integer Numbers"),
-        elevation: 1,
-      ),
-      body: Center( 
+  Center get _appBody => Center( 
         child: Container(
           height: 200,
           width: 200,
@@ -46,12 +47,18 @@ class _MyWidgetHomePageState extends State<MyWidgetHomePage> {
             borderRadius: BorderRadius.circular(10),
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Center children vertically
-            crossAxisAlignment: CrossAxisAlignment.center, // Center children horizontally
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[ _olaText, _randomIntText()]
           )
         )
-      ),
+      );
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar,
+      body: _appBody,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: (){
